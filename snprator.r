b942e0c0cec5807362bc1c6cb3b8fd8b7453a281
Arthur Korte <arthur.korte@gmail.com>
2020-04-18 12:24:55 +0200

    removed small typo

--- snprator.r
+++ snprator.r
@@ -39,9 +39,9 @@ j<-nrow(D)
 if(D[j,3]%in%SNPs$SNP==FALSE) { j=j-1} else {break}}
 
 
- if(SNPs[which(SNPs$SNP==D[j,3]),5]!=SNPs[which(SNPs$SNP==D[h,3]),5]) {
+ if(SNPs[which(SNPs$SNP==D[j,3]),5]!=SNPs[which(SNPs$SNP==D[h,3]),5]) 
  
-load(paste('X_',data,'_',SNPs[which(SNPs$SNP==D[nrow(D),3]),5],'.rda.gz',sep=''))
+load(paste('X_',data,'_',SNPs[which(SNPs$SNP==D[nrow(D),3]),5],'.rda',sep=''))
 XX<-cbind(XX,X[as.numeric(rownames(X))%in%accessions,colnames(X)%in%D$SNP])
 rm(X) }
 
